2228c3e7fb41d3f3c1a44fb391224ae8f891f766
Yiliang Xiong <wlxiong@gmail.com>
2012-08-07 14:53:11 +0800

    supplyment answers to Exercise 1.10

--- ex-1.10.scm
+++ ex-1.10.scm
@@ -40,3 +40,28 @@
 (A 2 (A 2 (A 3 1)))
 (A 2 4)
 65536
+
+
+(define (f n) (A 0 n))
+
+(define (g n) (A 1 n))
+
+(define (h n) (A 2 n))
+
+(define (ff n)
+	(* 2 n)
+)
+
+(define (gg n)
+	(if (= n 0)
+		1
+		(* 2 (gg (- n 1)))
+	)
+)
+
+(define (hh n)
+	(if (= n 0)
+		1
+		(gg (hh (- n 1)))
+	)
+)
